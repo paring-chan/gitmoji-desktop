b3dd672b3e4e358db4e00d2cde1665aa96168c08
Pariring <paring@pikokr.dev>
2023-01-13 13:47:16 +0900

    ✨ help page

--- src/renderer/src/views/Help.tsx
+++ src/renderer/src/views/Help.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
 export const HelpPage: React.FC = () => {
-  return <div className="p-4">Help</div>
+  return (
+    <div className="p-4">
+      <h2 className="text-3xl font-bold">Gitmoji Desktop</h2>
+      <div className="text-lg">
+        by{' '}
+        <a
+          href="https://github.com/pikokr"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-600"
+        >
+          파링
+        </a>
+      </div>
+
+      <h3 className="text-2xl mt-4 font-medium">Credits</h3>
+      <div className="flex flex-col text-blue-600">
+        <a href="https://gitmoji.dev" target="_blank" rel="noreferrer">
+          Gitmoji
+        </a>
+        <a href="https://toss.im/tossface" target="_blank" rel="noreferrer">
+          TossFace
+        </a>
+        <a href="https://tabler-icons-react.vercel.app/" target="_blank" rel="noreferrer">
+          Tabler Icons
+        </a>
+      </div>
+    </div>
+  )
 }
