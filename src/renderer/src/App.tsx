57bddbe658ec53fcc74dd1aafedb5a0028949d15
Pariring <paring@pikokr.dev>
2023-01-13 13:43:03 +0900

    🐛 invalid link position

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -5,9 +5,10 @@ function App(): JSX.Element {
   return (
     <div className="w-full h-screen flex flex-col">
       <div className="border-b p-4 flex">
-        <Link to="/" className="text-2xl font-bold flex-grow">
+        <Link to="/" className="text-2xl font-bold">
           Gitm😜ji
         </Link>
+        <div className="flex-grow" />
         <Link
           to="/help"
           className="border w-8 h-8 rounded-full flex justify-center items-center cursor-pointer hover:border-black transition-all"
