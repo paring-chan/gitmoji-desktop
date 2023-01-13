2a3027d81b7c7f338b677f711baa2b041a26633d
Pariring <paring@pikokr.dev>
2023-01-13 14:22:05 +0900

    ✨ block devtools

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,7 @@ import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import isDev from 'electron-is-dev'
 
 function createWindow(): void {
   // Create the browser window.
@@ -17,7 +18,8 @@ function createWindow(): void {
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: false,
+      devTools: isDev
     }
   })
 
