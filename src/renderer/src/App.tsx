3aaf991cfbad69a7568fbd88b922a3ebcaf1c042
Pariring <paring@pikokr.dev>
2023-01-13 13:26:47 +0900

    ✨ header

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,15 @@
+import { QuestionMark } from 'tabler-icons-react'
 import { GitmojiList } from './components/GitmojiList'
 
 function App(): JSX.Element {
   return (
     <div className="w-full h-screen flex flex-col">
+      <div className="border-b p-4 flex">
+        <div className="text-2xl font-bold flex-grow">Gitm😜ji</div>
+        <div className="border w-8 h-8 rounded-full flex justify-center items-center cursor-pointer hover:border-black transition-all">
+          <QuestionMark />
+        </div>
+      </div>
       <div className="flex-grow h-0 overflow-y-scroll">
         <GitmojiList />
       </div>
--- src/renderer/src/components/GitmojiItem.tsx
+++ src/renderer/src/components/GitmojiItem.tsx
@@ -5,9 +5,7 @@ export const GitmojiItem: React.FC<{ emoji: (typeof gitmojis)[0] }> = ({ emoji }
   return (
     <div className="rounded-md bg-white shadow-md mt-4 p-4">
       <div className="flex gap-4">
-        <div style={{ fontFamily: 'TossFace' }} className="text-4xl drop-shadow-lg">
-          {emoji.emoji}
-        </div>
+        <div className="text-4xl drop-shadow-lg">{emoji.emoji}</div>
         <div>
           <div className="text-xl">{emoji.code}</div>
           <div className="mt-2 text-sm">{emoji.description}</div>
