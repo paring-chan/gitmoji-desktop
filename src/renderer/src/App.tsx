52727eb22e955be1ea279adc4dcc7bbd540c84db
Pariring <paring@pikokr.dev>
2023-01-13 13:14:27 +0900

    ✨ gitmoji item design

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,8 +2,10 @@ import { GitmojiList } from './components/GitmojiList'
 
 function App(): JSX.Element {
   return (
-    <div className="w-full h-screen overflow-y-auto">
-      <GitmojiList />
+    <div className="w-full h-screen flex flex-col">
+      <div className="flex-grow h-0 overflow-y-scroll">
+        <GitmojiList />
+      </div>
     </div>
   )
 }
--- src/renderer/src/components/GitmojiList.tsx
+++ src/renderer/src/components/GitmojiList.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { gitmojis } from 'gitmojis/src/gitmojis.json'
+import { GitmojiItem } from './GitmojiItem'
 
 export const GitmojiList: React.FC = () => {
-  return <div>list</div>
+  return (
+    <div className="p-4 pt-0">
+      {gitmojis.map((x, i) => (
+        <GitmojiItem key={i} emoji={x} />
+      ))}
+    </div>
+  )
 }
