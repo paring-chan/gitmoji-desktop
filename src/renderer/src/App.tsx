9839132c615251cc735db45418d89e0b71a81483
Pariring <paring@pikokr.dev>
2023-01-13 13:33:18 +0900

    ✨ setup router

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,22 @@
+import { Link, Outlet } from 'react-router-dom'
 import { QuestionMark } from 'tabler-icons-react'
-import { GitmojiList } from './components/GitmojiList'
 
 function App(): JSX.Element {
   return (
     <div className="w-full h-screen flex flex-col">
       <div className="border-b p-4 flex">
-        <div className="text-2xl font-bold flex-grow">Gitm😜ji</div>
-        <div className="border w-8 h-8 rounded-full flex justify-center items-center cursor-pointer hover:border-black transition-all">
+        <Link to="/" className="text-2xl font-bold flex-grow">
+          Gitm😜ji
+        </Link>
+        <Link
+          to="/help"
+          className="border w-8 h-8 rounded-full flex justify-center items-center cursor-pointer hover:border-black transition-all"
+        >
           <QuestionMark />
-        </div>
+        </Link>
       </div>
       <div className="flex-grow h-0 overflow-y-scroll">
-        <GitmojiList />
+        <Outlet />
       </div>
     </div>
   )
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
+import { GitmojiList } from './components/GitmojiList'
 import './stylesheets/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<GitmojiList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
